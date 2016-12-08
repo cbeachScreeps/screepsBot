b69e39da201403b41f11ac132afed359ea197cce
Casey Beach <casey.beach@regence.com>
2016-12-07 16:48:39 -0800

    Dail back how fast creeps are created

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-const C = require('constants');
 const utils = require('utilities');
 const PriorityQueue = require('PriorityQueue');
 const roleWorker = require('role.worker');
-const workOrders = require('work_orders');
-const fillOrders = require('fill_orders');
-
 
 module.exports.loop = function () {
     console.log(`time: ${Game.time}`);
     for (var r in Game.rooms) {
         let room = Game.rooms[r];
-        if (room.memory.queue) {
-            room.memory.queue = new PriorityQueue(priorityFunction);
+        utils.getEnergyCollectionPoints(room)
+        if (Game.rooms[r].memory.queue) {
+            Game.rooms[r].memory.queue = new PriorityQueue(priorityFunction);
         }
-        // Check
-        workOrders(room);
-        fillOrders(room);
         //var workers = _.filter(Game.creeps, (creep) => creep.memory.role == 'worker');
         let cPs = utils.getEnergyCollectionPoints(room)
         console.log(`cPs.length: ${cPs.length}`);
         //if (workers.length < 7) { //Game.rooms[room].memory.collectionPoints.energy.length) {
         if (cPs.length > 0) {
-            console.log('spawning worker');
-            Game.spawns['Spawn1'].createCreep([MOVE, CARRY, WORK, WORK], undefined, {role: 'worker'});
+            if (!room.memory.cPTurnsEmpty) {
+                room.memory.cPTurnsEmpty = 0;
+            } else if (room.memory.cPTurnsEmpty > 3) {
+                room.memory.cPTurnsEmpty++;
+                console.log('spawning worker');
+                Game.spawns['Spawn1'].createCreep([MOVE, CARRY, WORK, WORK], undefined, {role: 'worker'});    
+            }
+        } else {
+            room.memory.cPTurnsEmpty = 0;
         }
     }
   
